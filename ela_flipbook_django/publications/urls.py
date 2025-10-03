5bf9fef55068a15414079bbc719c353b04358939
nigel2421 <nigel2421@yahoo.com>
2025-10-03 12:03:43 +0300

    hero

--- ela_flipbook_django/publications/urls.py
+++ ela_flipbook_django/publications/urls.py
@@ -3,9 +3,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'publications' # This is for namespacing
+app_name = 'publications'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    
+    # New URLs for our pages
+    path('magazine/', views.magazine_view, name='magazine'),
+    path('articles/', views.articles_view, name='articles'),
+    path('contact/', views.contact_view, name='contact'),
+    
     path('publication/<int:pk>/', views.publication_detail_view, name='detail'),
 ]
--- ela_flipbook_django/publications/views.py
+++ ela_flipbook_django/publications/views.py
@@ -3,25 +3,50 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Publication
 
+# --- Existing Views ---
 def home_view(request):
-    """
-    This view gets all publications from the database and sends them
-    to the home.html template.
-    """
-    publications = Publication.objects.all()
+    all_publications = Publication.objects.all()
+    latest_publications = Publication.objects.order_by('-uploaded_at')[:3]
     context = {
-        'publications': publications
+        'all_publications': all_publications,
+        'latest_publications': latest_publications
     }
     return render(request, 'publications/home.html', context)
 
-
 def publication_detail_view(request, pk):
-    """
-    This view gets a single publication by its unique ID (pk) and sends it
-    to the publication_detail.html template.
-    """
     publication = get_object_or_404(Publication, pk=pk)
     context = {
         'publication': publication
     }
     return render(request, 'publications/publication_detail.html', context)
+
+# --- NEW VIEWS ---
+
+def magazine_view(request):
+    """
+    This view will display all publications, just like the grid on the homepage.
+    We will create a separate template for it.
+    """
+    publications = Publication.objects.all()
+    context = {
+        'publications': publications
+    }
+    return render(request, 'publications/magazine.html', context)
+
+def articles_view(request):
+    """
+    This view also displays all publications. In the future, you could
+    add a filter here for a specific category, e.g., "Article".
+    """
+    publications = Publication.objects.all()
+    context = {
+        'publications': publications
+    }
+    return render(request, 'publications/articles.html', context)
+
+def contact_view(request):
+    """
+    This view just needs to render the contact page template.
+    """
+    # You can add context here later, like your email address or phone number
+    return render(request, 'publications/contact.html')
