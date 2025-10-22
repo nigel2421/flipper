dc4dbc3234b321e6e86f953a67f960b3c480060e
nigel2421 <nigel2421@yahoo.com>
2025-10-22 16:43:41 +0300

    jua niku baya

--- ela_flipbook_django/publications/admin.py
+++ ela_flipbook_django/publications/admin.py
@@ -3,9 +3,14 @@
 from django.contrib import admin
 from .models import Publication
 
+# We use a ModelAdmin class to customize the admin interface
 class PublicationAdmin(admin.ModelAdmin):
+    # This controls the columns you see in the main list of publications.
     list_display = ('title', 'uploaded_at')
-    # The cover_image is generated automatically, so we don't need to edit it
-    readonly_fields = ('cover_image',)
+    
+    # This explicitly defines which fields appear on the "Add/Change" form
+    # and in what order. By including 'cover_image' here, we force it to appear.
+    fields = ('title', 'pdf_file', 'cover_image')
 
+# Register your Publication model with the customized PublicationAdmin class
 admin.site.register(Publication, PublicationAdmin)
