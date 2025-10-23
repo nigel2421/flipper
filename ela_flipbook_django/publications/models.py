814f836e3aa2825ee405ab66c551132b9e7940ae
nigel2421 <nigel2421@yahoo.com>
2025-10-23 14:15:25 +0300

    next ni live

--- ela_flipbook_django/publications/models.py
+++ ela_flipbook_django/publications/models.py
@@ -1,6 +1,9 @@
 # publications/models.py
 
 from django.db import models
+from django.contrib.auth.models import User # Import the User model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Publication(models.Model):
     title = models.CharField(max_length=200)
@@ -17,3 +20,29 @@ class Publication(models.Model):
     class Meta:
         # This will order publications by most recently uploaded first
         ordering = ['-uploaded_at']
+
+        # --- NEW PROFILE MODEL ---
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+# --- NEW ROBUST SIGNAL ---
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Creates a Profile for a new user, or ensures an existing user has one.
+    """
+    if created:
+        # If a new user is created, also create their profile
+        Profile.objects.create(user=instance)
+    else:
+        # For existing users, check if they have a profile.
+        # Use a try-except block to handle cases where it's missing.
+        try:
+            instance.profile.save()
+        except Profile.DoesNotExist:
+            # If the profile does not exist for this old user, create it now.
+            Profile.objects.create(user=instance)
