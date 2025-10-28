795bed9de17d9bbc170d91f1135c724ef936dc4b
nigel2421 <wemisico@rs10.rcnoc.com>
2025-10-28 14:31:00 +0300

    newuser

--- wemisico/businessmatters.co.ke/ela_flipbook_django/flipbook_project/publications/admin.py
+++ wemisico/businessmatters.co.ke/ela_flipbook_django/flipbook_project/publications/admin.py
@@ -6,52 +6,49 @@ from django.contrib.auth.models import User
 import csv
 from django.http import HttpResponse
 
-from .models import Publication, Profile # Import the new Profile model
+# Import all your models
+from .models import Publication, Profile, Event
 
-# --- Publication Admin (no changes) ---
+# --- Publication Admin ---
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at')
     fields = ('title', 'pdf_file', 'cover_image')
 admin.site.register(Publication, PublicationAdmin)
 
 
-# --- NEW: Action to export users as CSV ---
+# --- Event Admin ---
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date')
+    fields = ('title', 'event_date', 'poster', 'caption')
+admin.site.register(Event, EventAdmin)
+
+
+# --- Custom User Admin with Profile Inline and CSV Export ---
+
+# Action to export users as CSV
 def export_as_csv(modeladmin, request, queryset):
-    """
-    A Django admin action to export selected users as a CSV file.
-    """
     meta = modeladmin.model._meta
     field_names = ['first_name', 'last_name', 'email', 'phone_number']
-
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = f'attachment; filename={meta.verbose_name_plural}.csv'
     writer = csv.writer(response)
-
     writer.writerow(field_names)
-    for user in queryset:
-        # Get phone number from the related profile
+    for user in queryset.select_related('profile'):
         phone_number = user.profile.phone_number if hasattr(user, 'profile') else ''
         writer.writerow([user.first_name, user.last_name, user.email, phone_number])
-
     return response
 export_as_csv.short_description = "Export Selected Users to CSV"
 
-
-# --- NEW: Custom User Admin ---
-
-# Define an inline admin descriptor for the Profile model
-# which acts a bit like a singleton
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
     verbose_name_plural = 'Profile'
     fk_name = 'user'
 
-# Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    actions = [export_as_csv] # Add the export action
+    actions = [export_as_csv]
 
 # Re-register UserAdmin
 admin.site.unregister(User)
