fdfe6b1fc7f5e7981372c7aa1d4db9c077eef7b7
nigel2421 <wemisico@rs10.rcnoc.com>
2025-10-31 13:42:13 +0300

    checkcheck

--- wemisico/businessmatters.co.ke/ela_flipbook_django/flipbook_project/publications/forms.py
+++ wemisico/businessmatters.co.ke/ela_flipbook_django/flipbook_project/publications/forms.py
@@ -2,7 +2,6 @@
 
 from django import forms
 from allauth.account.forms import SignupForm
-from django.contrib.auth.models import User
 from .models import Profile
 
 class CustomSignupForm(SignupForm):
@@ -11,32 +10,28 @@ class CustomSignupForm(SignupForm):
     phone_number = forms.CharField(max_length=20, label='Phone Number (Optional)', required=False, widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
 
     def save(self, request):
-        # Create the new user first
+        # Create the new user
         user = super(CustomSignupForm, self).save(request)
         
-        # Save the extra data from the form
+        # Save the extra data
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.save()
 
         # The profile is created automatically by our signal.
-        # We just need to update it.
         user.profile.phone_number = self.cleaned_data.get('phone_number')
         
-        # --- THIS IS THE NEW, MORE ROBUST LOGIC ---
-        # Get the referral code from the session if it exists
+        # --- THIS IS THE NEW, GUARANTEED LOGIC ---
+        # Get the referral code from the session
         referral_code = request.session.get('referral_code')
         if referral_code:
             try:
-                # Find the user who owns this referral code
                 referrer_profile = Profile.objects.get(referral_code=referral_code)
-                # Save the relationship
                 user.profile.referred_by = referrer_profile.user
-                # Clear the referral code from the session so it's only used once
+                # Important: Clear the code from the session so it's only used once
                 del request.session['referral_code']
             except Profile.DoesNotExist:
-                # If the code is invalid, do nothing
-                pass
+                pass # If the code is invalid, do nothing
 
         user.profile.save()
         
