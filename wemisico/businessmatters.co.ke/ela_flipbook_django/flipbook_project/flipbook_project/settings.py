6202b9662be961cff6dfd0416450c294660f04e5
nigel2421 <wemisico@rs10.rcnoc.com>
2025-11-15 23:32:01 +0300

    kataatu

--- wemisico/businessmatters.co.ke/ela_flipbook_django/flipbook_project/flipbook_project/settings.py
+++ wemisico/businessmatters.co.ke/ela_flipbook_django/flipbook_project/flipbook_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p)$_lf*$b=h2x(l9cv^r8yo(662fa^1=nbnfs+fe8(iw-x^c+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 
     'press.businessmatters.co.ke',
