c75779c22e7291c80185a1b5b493429a3c43ba77
nigel2421 <nigel2421@gmail.com>
2025-12-04 20:23:31 +0000

    check local

---
+++ ela_flipbook_django/dev.nix
@@ -0,0 +1,29 @@
+
+{ pkgs, ... }:
+
+{
+  # This list defines the tools for your development environment.
+  packages = [
+    pkgs.python3
+    pkgs.postgresql_15 # Specifies the PostgreSQL package
+  ];
+
+  # This section defines the background services to run.
+  services.postgresql = {
+    enable = true;      # This turns the database server on.
+    package = pkgs.postgresql_15;
+
+    # This script runs ONCE to set up your database.
+    # It creates the user and database that Django will connect to.
+    initialScript = pkgs.writeText "init-db" '''
+      CREATE ROLE ela_flipbook_user WITH LOGIN PASSWORD 'a_secure_password';
+      CREATE DATABASE ela_flipbook_db OWNER ela_flipbook_user;
+    ''';
+  };
+
+  # You can add commands here to run when your environment starts.
+  enterShell = ''
+    echo "Nix environment is ready. PostgreSQL service is running."
+  '';
+}
+
