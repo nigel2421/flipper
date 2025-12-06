51361afd1cdaa7048d277c0e557065243e727dcd
nigel2421 <nigel2421@gmail.com>
2025-12-06 11:47:18 +0000

    debug off working as of Dec 6 new checkpoint

--- ela_flipbook_django/flipbook_project/settings.py
+++ ela_flipbook_django/flipbook_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k@d95k@5!n9@&h&s*v^$@8&m!%2*#@c$v&d%d(e@#@b&b!@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'press.businessmatters.co.ke',
