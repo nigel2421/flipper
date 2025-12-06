4ea39a87c5f730a6dee4daaaea81f36437f9f920
nigel2421 <nigel2421@gmail.com>
2025-12-06 13:15:32 +0000

    dmin changes

--- ela_flipbook_django/publications/admin.py
+++ ela_flipbook_django/publications/admin.py
@@ -4,13 +4,67 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 import csv
+import datetime
 from django.http import HttpResponse
 
-# Import all your models
+# Import allauth models and admin for customization
+from allauth.account.models import EmailAddress
+from allauth.account.admin import EmailAddressAdmin as AllauthEmailAddressAdmin
+
+# Import your project's models
 from .models import Magazine, Article, Profile, Event, Author, Tag, Rating, Comment, CommentReport
 
 
-# --- NEW: Register Author, Tag, and Rating Models ---
+# --- Custom Admin Action to Export User Emails ---
+def export_emails_and_join_date_csv(modeladmin, request, queryset):
+    """
+    Exports user emails and their join dates to a CSV file.
+    If users are selected, only they are exported. Otherwise, all users are exported.
+    """
+    if not queryset.exists():
+        queryset = modeladmin.model.objects.all()
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename=user_emails_{datetime.date.today()}.csv'
+    writer = csv.writer(response)
+
+    writer.writerow(['Email', 'Date Joined'])
+
+    for user in queryset:
+        writer.writerow([user.email, user.date_joined.strftime('%Y-%m-%d')])
+
+    return response
+
+export_emails_and_join_date_csv.short_description = "Export Email & Join Date CSV"
+
+
+# --- NEW: Action to export from EmailAddress model ---
+def export_emailaddress_csv(modeladmin, request, queryset):
+    """
+    Exports email addresses and their associated user's join date to a CSV file.
+    If emails are selected, only they are exported. Otherwise, all emails are exported.
+    """
+    if not queryset.exists():
+        queryset = modeladmin.model.objects.all()
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename=all_email_addresses_{datetime.date.today()}.csv'
+    writer = csv.writer(response)
+
+    writer.writerow(['Email', 'User Joined Date', 'Is Primary'])
+
+    # Eager load the related user to make it more efficient
+    for email_address in queryset.select_related('user'):
+        join_date = email_address.user.date_joined.strftime('%Y-%m-%d')
+        writer.writerow([email_address.email, join_date, email_address.primary])
+
+    return response
+
+export_emailaddress_csv.short_description = "Export Selected Emails & Join Dates to CSV"
+
+
+# --- Model Admins ---
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'get_user_email')
@@ -18,15 +72,13 @@ class AuthorAdmin(admin.ModelAdmin):
     autocomplete_fields = ['user']
 
     def get_user_email(self, obj):
-        if obj.user:
-            return obj.user.email
-        return '-'
+        return obj.user.email if obj.user else '-'
     get_user_email.short_description = 'User Email'
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
-    prepopulated_fields = {'slug': ('name',)} # Auto-fills slug from name
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
@@ -38,13 +90,12 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'article', 'text', 'created_at', 'is_reported', 'report_count')
     list_filter = ('is_reported', 'created_at', 'article__title')
     search_fields = ('user__username', 'article__title', 'text')
-    actions = ['mark_as_reviewed', 'mark_as_not_reported']
+    actions = ['mark_as_reviewed']
 
     def mark_as_reviewed(self, request, queryset):
         queryset.update(is_reported=False, report_count=0)
         self.message_user(request, "Selected comments marked as reviewed.")
     mark_as_reviewed.short_description = "Mark selected comments as reviewed"
-
 
 @admin.register(CommentReport)
 class CommentReportAdmin(admin.ModelAdmin):
@@ -58,74 +109,26 @@ class CommentReportAdmin(admin.ModelAdmin):
         self.message_user(request, "Selected reports marked as resolved.")
     mark_as_resolved.short_description = "Mark selected reports as resolved"
 
-
-# --- Magazine Admin (for Flipbooks) ---
 @admin.register(Magazine)
 class MagazineAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at')
     fields = ('title', 'excerpt', 'pdf_file', 'cover_image')
 
-# --- NEW: Article Admin ---
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'uploaded_at', 'is_featured', 'is_editors_pick', 'view_count')
     list_filter = ('is_featured', 'is_editors_pick', 'tags', 'author')
-    search_fields = ('title', 'excerpt', 'author__name', 'author__user__username', 'author__user__email')
+    search_fields = ('title', 'excerpt', 'author__name', 'author__user__username')
     autocomplete_fields = ['author']
     filter_horizontal = ('tags',)
-    # Define the fields for the "Add/Change" page
     fields = ('title', 'author', 'excerpt', 'content', 'cover_image', 'tags', 'is_featured', 'is_editors_pick')
 
-
-# --- Event Admin ---
+@admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'event_date')
     fields = ('title', 'event_date', 'poster', 'caption')
-admin.site.register(Event, EventAdmin)
-
 
 # --- Custom User Admin with Profile Inline and CSV Export ---
-
-# --- UPDATED: Action to export users as CSV ---
-def export_as_csv(modeladmin, request, queryset):
-    """
-    Exports selected users with their profile info, signup date, and referrer.
-    """
-    meta = modeladmin.model._meta
-    # Add the new fields you want to export
-    field_names = ['first_name', 'last_name', 'email', 'phone_number', 'signup_date', 'referred_by']
-
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = f'attachment; filename={meta.verbose_name_plural}.csv'
-    writer = csv.writer(response)
-
-    writer.writerow(field_names)
-    
-    # Eager load the related profile to make it more efficient
-    for user in queryset.select_related('profile'):
-        # Get phone number from the related profile
-        phone_number = user.profile.phone_number if hasattr(user, 'profile') else ''
-        
-        # Get the signup date from the user model
-        signup_date = user.date_joined.strftime('%Y-%m-%d %H:%M:%S')
-        
-        # Get the referrer's email from the profile, if it exists
-        referred_by_email = ''
-        if hasattr(user, 'profile') and user.profile.referred_by:
-            referred_by_email = user.profile.referred_by.email
-
-        writer.writerow([
-            user.first_name, 
-            user.last_name, 
-            user.email, 
-            phone_number, 
-            signup_date, 
-            referred_by_email
-        ])
-
-    return response
-export_as_csv.short_description = "Export Selected Users to CSV"
-
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
@@ -135,14 +138,24 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    actions = [export_as_csv]
+    actions = [export_emails_and_join_date_csv]
 
     def get_inline_instances(self, request, obj=None):
-        # Return no inlines when creating a new user
         if not obj:
-            return list()
+            return []
         return super().get_inline_instances(request, obj)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+
+# --- NEW: Custom EmailAddress Admin with CSV Export ---
+class CustomEmailAddressAdmin(AllauthEmailAddressAdmin):
+    # Inherits list_display, search_fields etc. from the original admin
+    # and adds our custom action.
+    actions = [export_emailaddress_csv]
+
+# Unregister the default EmailAddress admin and register our custom one
+admin.site.unregister(EmailAddress)
+admin.site.register(EmailAddress, CustomEmailAddressAdmin)
