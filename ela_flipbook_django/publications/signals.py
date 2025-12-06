46b4375a574dacfc6756c57b5550a4c6a92ef993
nigel2421 <nigel2421@gmail.com>
2025-12-06 10:22:01 +0000

    root cause of the RelatedObjectDoesNotExist error fixed

--- ela_flipbook_django/publications/signals.py
+++ ela_flipbook_django/publications/signals.py
@@ -6,8 +6,7 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """Create or update the user profile."""
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create a user profile when a new user is created."""
     if created:
         Profile.objects.create(user=instance)
-    instance.profile.save()
