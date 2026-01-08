b285a1998bfd52934b820850f6dbfd72a9ee33ff
nigel2421 <nigel2421@yahoo.com>
2026-01-08 11:59:25 +0300

    passenger dubugger

--- ela_flipbook_django/passenger_wsgi.py
+++ ela_flipbook_django/passenger_wsgi.py
@@ -1,6 +1,12 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(__file__))
+try:
+    sys.path.insert(0, os.path.dirname(__file__))
 
-from flipbook_project.wsgi import application
+    from flipbook_project.wsgi import application
+except Exception:
+    import traceback
+    with open('passenger_report.log', 'w') as f:
+        f.write(traceback.format_exc())
+    raise
