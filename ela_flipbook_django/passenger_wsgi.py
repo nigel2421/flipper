39c845fb6af956779dd574e92fecda5abf43d2ad
nigel2421 <nigel2421@yahoo.com>
2026-01-08 12:04:08 +0300

    hope its done sasas

--- ela_flipbook_django/passenger_wsgi.py
+++ ela_flipbook_django/passenger_wsgi.py
@@ -1,12 +1,6 @@
 import os
 import sys
 
-try:
-    sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(__file__))
 
-    from flipbook_project.wsgi import application
-except Exception:
-    import traceback
-    with open('passenger_report.log', 'w') as f:
-        f.write(traceback.format_exc())
-    raise
+from flipbook_project.wsgi import application
