cdfe5d2342f3d6d730f71620064ae7ab898d34cc
nigel2421 <nigel2421@gmail.com>
2026-02-28 09:49:16 +0000

    twende anti

--- ela_flipbook_django/flipbook_project/settings.py
+++ ela_flipbook_django/flipbook_project/settings.py
@@ -27,10 +27,21 @@ SECRET_KEY = 'django-insecure-k@d95k@5!n9@&h&s*v^$@8&m!%2*#@c$v&d%d(e@#@b&b!@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'press.businessmatters.co.ke',
+    'businessmatters.co.ke',
+    '127.0.0.1',
+    'localhost',
+    'www.businessmatters.co.ke',
+    # Add the development server host
+    '8000-firebase-flippergit-1764678684466.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
+    'idx-flippergit-09470411-122195396017.africa-south1.run.app',
+    'idx-flippergit-09470411-cydpcotz4q-bq.a.run.app/',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-firebase-flippergit-1764678684466.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
+    'https://idx-flippergit-09470411-122195396017.africa-south1.run.app',
 ]
 
 SITE_ID = 1
